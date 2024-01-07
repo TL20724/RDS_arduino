330a849da86e942195ef035b2d4a51ea30ba69f2
TL20724 <155886705+TL20724@users.noreply.github.com>
2024-01-07 13:39:47 +0200

    Update rds_lcd.ino
    Προγραμματισμὸς τῆς ὁθόνης να ἐμφανίζει ἕνα κείμενο.

--- rds_lcd.ino
+++ rds_lcd.ino
@@ -1,6 +1,13 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#define I2C_ADDR 0x27
+LiquidCrystal_I2C lcd(I2C_ADDR, 16, 2);
 void setup() {
-}
-
-void loop() {
-  
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("  Radio Tei FM");
+  lcd.setCursor(0, 1);
+  lcd.print("Paizei twra: ---");
 }
